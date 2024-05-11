6a21c14e5c0247e25e8ec48f37e7bce647bb7e49
Farhad2590 <farhadhossen2590@gmail.com>
2024-05-11 17:19:33 +0600

    Cards Data Updated In Rooms Page

--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -6,10 +6,12 @@ const Rooms = () => {
     const data = useLoaderData()
     console.log(data);
     return (
-        <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {
-                data.map(rooms => <RoomsCard rooms={rooms}></RoomsCard>)
-            }
+        <div className = ' container px-6 py-10 mx-auto'>
+            <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {
+                    data.map(rooms => <RoomsCard rooms={rooms}></RoomsCard>)
+                }
+            </div>
         </div>
     );
 };
